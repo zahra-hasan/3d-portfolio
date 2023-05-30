9430f7cd7763182c6e5972dfa689df69e8f37a0e
Adewale Ojuko <zahra.hasan.hilal@gmail.com>
2023-05-30 14:05:43 +0100

    added navbar code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+//import router not sure what it does just yet
+import { BrowserRouter } from 'react-router-dom';
+//import the sections.jsx to the main app.jsx from the components folder
+import { About, Contact, Experience, Feedbacks, 
+  Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
+//the main function that builds the page
 const App = () => {
   return (
-    <div>
-      3D Developer Portfolio 
-    </div>
+    //wrap everything inside a BrowserRouter component to allow "routing"?
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat
+        bg-center'>
+          <Navbar/>
+          <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className='relative z-0'>
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,83 @@
-import React from 'react'
-
+import React, { useEffect, useState} from 'react';
+import {Link } from 'react-router-dom';
+import { styles} from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
+ 
 const Navbar = () => {
+  const [active, setActive] = useState("") //initial state is empty string
+  const [toggle, setToggle] = useState(false) //initial sm menue not toggled
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5
+     fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex items-center justify-between max-w-7xl mx-auto'>
+        <Link to='/' //Link and not link !!!
+        className='flex items-center gap-2'
+        onClick={() => {
+          setActive(""); //function to keep track of where we are currently in the page 
+          window.scrollTo(0,0); //onclick it will scroll to the top of the page 
+        }}>
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px]
+          font-bold cursor-pointer flex '>
+            ZAHRAHSAN &nbsp;
+            <span className='sm:block hidden'> | ART</span>
+          </p>
+        </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title
+                  ? "text-white"
+                  : "text-secondary"
+                } hover:text-white text-[18px]
+                font-medium cursor-pointer`}
+                onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>
+                {link.title}
+              </a>
+            </li>
+          ) 
+          )}
+        </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+          src={ toggle? close : menu} 
+          alt="mneu" 
+          className='w-[28px] h-[28px] object-contain cursor-pointer'
+          onClick={() => setToggle(!toggle)} 
+          />
+          <div className={`${!toggle ? 'hidden' :'flex'} p-6 black-gradient absolute
+          top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+             <ul className='list-none flex justify-end
+             items-start flex-col gap-4'>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title
+                  ? "text-white"
+                  : "text-secondary"
+                } font-poppins font-medium cursor-pointer text-[16px]`}
+                onClick={() => {
+                  setActive(link.title);
+                  setToggle(!toggle);
+                }}
+            >
+              <a href={`#${link.id}`}>
+                {link.title}
+              </a>
+            </li>
+          ) 
+          )}
+        </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
